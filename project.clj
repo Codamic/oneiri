079badad9da5113b43f103bced505127a6bd1fe3
Sameer Rahmani <lxsameer@gnu.org>
2017-09-23 21:59:08 +0100

    app restructed to follow re-frame standards

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject oneiri "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure       "1.8.0"]
+  :dependencies [[org.clojure/clojure       "1.9.0-alpha20"]
                  [org.clojure/clojurescript "1.9.908"]
                  [codamic/hellhound         "1.0.0-SNAPSHOT"]]
 
